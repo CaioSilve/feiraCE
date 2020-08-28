5781b1c1a0947aebffcc39fa29a34f2679af7645
ErickNeves <erick.g.neves1265@gmail.com>
2020-08-27 22:38:25 -0300

    pao

--- feiraCE/src/views/testeCliente.java
+++ feiraCE/src/views/testeCliente.java
@@ -18,16 +18,16 @@ public class testeCliente {
 		
 		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); 
 		
-		Date data = formato.parse("09/06/2002");
+		Date data = formato.parse("25/02/1997");
 		
-		//Cliente clie = new Cliente(nome, nasc, rg, cpf, tele, cele, ende, cida, esta);
+		Cliente clie = new Cliente("Erick", data, "112345", "437031978-11", "981901770", "981901770", "Av:E 1265", "Orlândia", "SP");
 		
 		//CONSULTA
-//		List<Cliente> clies = dao.consultar("obterNome", "nome", "%veira%");
-//		
-//		for (Cliente cliente : clies) {
-//			JOptionPane.showMessageDialog(null, cliente.getNome());
-//		}
+		List<Cliente> clies = dao.consultar("obterNome", "nome", "%veira%");
+		
+		for (Cliente cliente : clies) {
+			JOptionPane.showMessageDialog(null, cliente.getNome());
+		}
 		
 		
 		
