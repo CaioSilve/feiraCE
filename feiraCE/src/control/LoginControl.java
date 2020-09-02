691a468c3d7f93a174d3802eb8caebe420a1a295
CaioSilve <caio.evil.silveira@gmail.com>
2020-09-02 20:28:08 -0300

    JFoenix

--- feiraCE/src/control/LoginControl.java
+++ feiraCE/src/control/LoginControl.java
@@ -2,12 +2,13 @@ package control;
 
 import javax.swing.JOptionPane;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
 import dao.DAO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import model.Usuario;
@@ -17,10 +18,10 @@ public class LoginControl {
 	DAO<Usuario> dao = new DAO<>(Usuario.class); 
 
 	@FXML
-	public TextField txtUser;
+	public JFXTextField txtUser;
 	
 	@FXML
-	public PasswordField txtPass;
+	public JFXPasswordField txtPass;
 	
 	public void entrar() throws Exception {	
 		if (txtUser.getText().isEmpty()) {
